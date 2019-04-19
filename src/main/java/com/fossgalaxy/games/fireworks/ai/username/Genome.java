311a692ba662390ae0a23fcbf766766befc5f91e
Will Price <pricew@southwestern.edu>
2019-04-19 12:35:01 -0500

    Everything should work now.

--- src/main/java/com/fossgalaxy/games/fireworks/ai/username/Genome.java
+++ src/main/java/com/fossgalaxy/games/fireworks/ai/username/Genome.java
@@ -2,9 +2,10 @@ import java.lang.Math;
 import java.util.Random;
 import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Collections;
 
 // one class needs to have a main() method
-public class Genome
+public class Genome implements Comparable<Genome>
 {
   double[] genome;
   double fitness;
@@ -35,11 +36,12 @@ public class Genome
       this.fitness = fitness;
   }
     
-  // arguments are passed using the text field below this editor
   public static void testMethods()
   {
     System.out.println("Testing vectorMutate:\nPass? " + testVectorMutate() + "\n");
-    System.out.println("Testing tournamentSelection:\nPass? " + testTournamentSelection());
+    System.out.println("Testing tournamentSelection:\nPass? " + testTournamentSelection() + "\n");
+	System.out.println("Testing generateOffspring:\nPass? " + testGenerateOffspring() + "\n");
+	System.out.println("Testing nextGeneration:\nPass? " + testNextGeneration() + "\n");
   }
 
   public static double[] vectorMutate(double[] genome, double mutationRate, double sigma){
@@ -96,19 +98,62 @@ public class Genome
 	return bestGenome.fitness == 100;
   }
 	
-  public static ArrayList<Genome> nextGeneration(ArrayList<Genome> population, double mutationRate) { // Elitism was in the paramater with default value of 0.1 (cant do this in java??)
-    double elitism = 0.1;
-    ArrayList<Genome> childpop;
-    ArrayList<Genome> populationCopy = (ArrayList<Genome>) population.clone(); // copy so as to not change the original
-    populationCopy = populationCopy.sort(); // //compareToMethod // sort an array list based off of the genomes second element
+  public static ArrayList<Genome> nextGeneration(ArrayList<Genome> population, double mutationRate, double sigma, double elitism) { 
+    ArrayList<Genome> childpop = new ArrayList<Genome>();
+    //ArrayList<Genome> populationCopy = (ArrayList<Genome>) population.clone(); // copy so as to not change the original
+    //population = Collections.sort(population);
     int valsToCopy = (int) Math.floor(elitism*(population.size()));
     for (int i = 0; i < valsToCopy; i++) {
-      childpop.add(populationCopy.get(i));
+      childpop.add(population.get(i));
     }
     for (int j = valsToCopy; j < population.size(); j++) {
-      childpop.add(getOffspring(population, mutationRate));
+      childpop.add(generateOffspring(population, mutationRate, sigma));
     }
     return childpop;
+  }
+  public static boolean testNextGeneration(){
+	double[] genome1 = {100};
+	double[] genome2 = {200};
+	double[] genome3 = {300};
+	double[] genome4 = {400};
+	double[] genome5 = {500};
+	double fitness1 = 100;
+	double fitness2 = 200;
+	double fitness3 = 300;
+	double fitness4 = 400;
+	double fitness5 = 500;
+	Genome organism1 = new Genome(genome1, fitness1);
+	Genome organism2 = new Genome(genome2, fitness2);
+	Genome organism3 = new Genome(genome3, fitness3);
+	Genome organism4 = new Genome(genome4, fitness4);
+	Genome organism5 = new Genome(genome5, fitness5);
+	ArrayList<Genome> population = new ArrayList<Genome>();
+	population.add(organism1);
+	population.add(organism2);
+	population.add(organism3);
+	population.add(organism4);
+	population.add(organism5);
+	ArrayList<Genome> test1 = nextGeneration(population, 1.0, 0.0, 1.0);
+	ArrayList<Genome> test2 = nextGeneration(population, 1.0, 100.0, 0.0);
+	
+	boolean test1Pass = true;
+	if(test1.get(0).genome[0] != 100){
+		test1Pass = false;
+	}
+	if(test1.get(1).genome[0] != 200){
+		test1Pass = false;
+	}
+	if(test1.get(2).genome[0] != 300){
+		test1Pass = false;
+	}
+	if(test1.get(3).genome[0] != 400){
+		test1Pass = false;
+	}
+	if(test1.get(4).genome[0] != 500){
+		test1Pass = false;
+	}
+	
+	return test1Pass;
   }
 	
   public static Genome generateOffspring(ArrayList<Genome> population){
