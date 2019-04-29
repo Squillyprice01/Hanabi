2642f4ba2f22c625bf2ea2ec98899cfefc539158
Will Price <Squillyprice01@users.noreply.github.com>
2019-04-29 15:44:33 -0500

    added GenerateGenration and created generation1

---
+++ src/main/java/util/GenerateGeneration.java
@@ -0,0 +1,31 @@
+package util;
+
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Random;
+
+public class GenerateGeneration {
+	static int numberOfGenesInGenome = 8;
+	static int numberOfIndividuals = 100;
+	
+	public static void main(String[] args) {
+		Random random = new Random();
+        PrintStream f = null;
+     	try {
+     		f = new PrintStream("./generation1.txt");
+     		System.setOut(f);
+     		for(int j = 0 ; j < numberOfIndividuals; j++) {
+	     		for(int i = 0; i < numberOfGenesInGenome; i++) {
+	     			if(i == numberOfGenesInGenome - 1) {
+	     				System.out.println(random.nextDouble());
+	     			} else {
+	     				System.out.print(random.nextDouble() + ",");
+	     			}
+	     		}
+     		}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
